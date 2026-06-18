8ba9ac9571e717d05db41594f382478b79fa6c42
Dankord <ganzonjohnkervin@email.com>
2026-06-18 08:20:11 +0800

    add cors config for frontend

---
+++ backend/config/cors.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://adoption-system-4h8it9uqn.vercel.app',
+        'http://localhost:3000',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
